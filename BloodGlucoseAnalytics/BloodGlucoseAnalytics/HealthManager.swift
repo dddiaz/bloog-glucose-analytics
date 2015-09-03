1dcc6c704d09f6827a8456695606646dfe54811e
Daniel Diaz <daniel.delvin.diaz@gmail.com>
2015-09-02 22:26:11 -0700

    healthkit authoriziation now working
    fix for set not working on healthstore authorization fix for this at:
    http://blog.appliedinformaticsinc.com/how-to-grant-readwrite-access-to-a
    pples-healthkit/

--- BloodGlucoseAnalytics/BloodGlucoseAnalytics/HealthManager.swift
+++ BloodGlucoseAnalytics/BloodGlucoseAnalytics/HealthManager.swift
@@ -1,13 +1,73 @@
 //
 //  HealthManager.swift
-//  HKTutorial
-//
-//  Created by ernesto on 18/10/14.
-//  Copyright (c) 2014 raywenderlich. All rights reserved.
-//
+//Based off this tutorial http://www.raywenderlich.com/86336/ios-8-healthkit-swift-getting-started
 
+import HealthKit
 import Foundation
 
 class HealthManager {
-  
+    let healthKitStore:HKHealthStore = HKHealthStore()
+    
+    func authorizeHealthKit(completion: ((success:Bool, error:NSError!) -> Void)!)
+    {
+        // 1. Set the types you want to read from HK Store
+//        let healthKitTypesToRead = Set(arrayLiteral:[
+//            HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierDateOfBirth),
+//            HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierBloodType),
+//            HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierBiologicalSex),
+//            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMass),
+//            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeight),
+//            //Added blood glucose permissions
+//            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBloodGlucose),
+//            HKObjectType.workoutType()
+//            ])
+        
+        let healthKitTypesToRead = NSSet(array: [
+            HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierDateOfBirth),
+            HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierBloodType),
+            HKObjectType.characteristicTypeForIdentifier(HKCharacteristicTypeIdentifierBiologicalSex),
+            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMass),
+            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeight),
+            //Added blood glucose permissions
+            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBloodGlucose),
+            HKObjectType.workoutType()
+        ])
+        
+        // 2. Set the types you want to write to HK Store
+//        let healthKitTypesToWrite = Set(arrayLiteral:[
+//            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMassIndex),
+//            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierActiveEnergyBurned),
+//            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning),
+//            HKQuantityType.workoutType()
+//            ])
+        
+        let healthKitTypesToWrite = NSSet(array: [
+            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMassIndex),
+            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierActiveEnergyBurned),
+            HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierDistanceWalkingRunning),
+            HKQuantityType.workoutType()
+        ])
+        
+        // 3. If the store is not available (for instance, iPad) return an error and don't go on.
+        if !HKHealthStore.isHealthDataAvailable()
+        {
+            let error = NSError(domain: "com.raywenderlich.tutorials.healthkit", code: 2, userInfo: [NSLocalizedDescriptionKey:"HealthKit is not available in this Device"])
+            if( completion != nil )
+            {
+                completion(success:false, error:error)
+            }
+            return;
+        }
+        
+        //fix for this at: http://blog.appliedinformaticsinc.com/how-to-grant-readwrite-access-to-apples-healthkit/
+        
+        // 4.  Request HealthKit authorization
+        healthKitStore.requestAuthorizationToShareTypes(healthKitTypesToWrite as Set, readTypes: healthKitTypesToRead as Set) { (success, error) -> Void in
+            
+            if( completion != nil )
+            {
+                completion(success:success,error:error)
+            }
+        }
+    }
 }
--- BloodGlucoseAnalytics/BloodGlucoseAnalytics/MasterViewController.swift
+++ BloodGlucoseAnalytics/BloodGlucoseAnalytics/MasterViewController.swift
@@ -12,48 +12,60 @@ import UIKit
 
 
 class MasterViewController: UITableViewController {
-  
-  let kAuthorizeHealthKitSection = 2
-  let kProfileSegueIdentifier = "profileSegue"
-  let kWorkoutSegueIdentifier = "workoutsSeque"
-  
-  let healthManager:HealthManager = HealthManager()
-  
-  
-  func authorizeHealthKit()
-  {
-    println("TODO: Request HealthKit authorization")
-  }
-  
-  
-  // MARK: - Segues
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    if segue.identifier ==  kProfileSegueIdentifier {
-      
-      if let profileViewController = segue.destinationViewController as? ProfileViewController {
-        profileViewController.healthManager = healthManager
-      }
-    }
-    else if segue.identifier == kWorkoutSegueIdentifier {
-      if let workoutViewController = segue.destinationViewController.topViewController as? WorkoutsTableViewController {
-        workoutViewController.healthManager = healthManager;
-      }
-    }
-  }
-  
-  // MARK: - TableView Delegate
-  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
     
-    switch (indexPath.section, indexPath.row)
+    let kAuthorizeHealthKitSection = 2
+    let kProfileSegueIdentifier = "profileSegue"
+    let kWorkoutSegueIdentifier = "workoutsSeque"
+  
+    let healthManager:HealthManager = HealthManager()
+  
+  
+    func authorizeHealthKit()
     {
-    case (kAuthorizeHealthKitSection,0):
-      authorizeHealthKit()
-    default:
-      break
+        // println("TODO: Request HealthKit authorization")
+        healthManager.authorizeHealthKit { (authorized,  error) -> Void in
+            if authorized {
+                println("HealthKit authorization received.")
+            }
+            else
+            {
+                println("HealthKit authorization denied!")
+                if error != nil {
+                    println("\(error)")
+                }
+            }
+        }
     }
-    self.tableView.deselectRowAtIndexPath(indexPath, animated: true)
-  }
   
   
-  
+    // MARK: - Segues
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier ==  kProfileSegueIdentifier {
+            
+            if let profileViewController = segue.destinationViewController as? ProfileViewController {
+                profileViewController.healthManager = healthManager
+            }
+        }
+        else if segue.identifier == kWorkoutSegueIdentifier {
+            if let workoutViewController = segue.destinationViewController.topViewController as? WorkoutsTableViewController {
+                workoutViewController.healthManager = healthManager;
+            }
+        }
+    }
+    
+    // MARK: - TableView Delegate
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        switch (indexPath.section, indexPath.row)
+        {
+        case (kAuthorizeHealthKitSection,0):
+            authorizeHealthKit()
+        default:
+            break
+        }
+        self.tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    
+    
 }
