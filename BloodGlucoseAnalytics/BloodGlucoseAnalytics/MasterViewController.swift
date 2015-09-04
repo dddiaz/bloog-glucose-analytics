30c0a5e0291f543abc6d371067a4b854f6868801
Daniel Diaz <daniel.delvin.diaz@gmail.com>
2015-09-03 23:15:56 -0700

    last blood glucose reading now working!!!

--- BloodGlucoseAnalytics/BloodGlucoseAnalytics/MasterViewController.swift
+++ BloodGlucoseAnalytics/BloodGlucoseAnalytics/MasterViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-
+import HealthKit
 import UIKit
 
 
@@ -16,10 +16,58 @@ class MasterViewController: UITableViewController {
     let kAuthorizeHealthKitSection = 2
     let kProfileSegueIdentifier = "profileSegue"
     let kWorkoutSegueIdentifier = "workoutsSeque"
-  
+    let kUnknownString   = "Unknown"
     let healthManager:HealthManager = HealthManager()
+    let mgdlUnit = HKUnit(fromString: "mg/dL")
   
-  
+    @IBOutlet var lastBgLabel: UILabel!
+    @IBOutlet weak var lastBGValueLabel: UILabel!
+    
+    var BG:HKQuantitySample?
+    
+    //set value of lastbg
+    //self.lastBgValueLabel.text = "100"
+    
+    func updateBGValues(){
+        updateLastBG()
+    }
+    
+    func updateLastBG(){
+        lastBgLabel.text = String("Last BG: 100")
+        
+        // 1. Construct an HKSampleType for weight
+        let sampleType = HKSampleType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBloodGlucose)
+        
+        // 2. Call the method to read the most recent weight sample
+        self.healthManager.readMostRecentSample(sampleType, completion: { (mostRecentBG, error) -> Void in
+            
+            if( error != nil )
+            {
+                println("Error reading BG from HealthKit Store: \(error.localizedDescription)")
+                return;
+            }
+            
+            var glucoLocalizedString = self.kUnknownString;
+            // 3. Format the weight to display it on the screen
+            self.BG = mostRecentBG as? HKQuantitySample;
+            
+            if let mgdl = self.BG?.quantity.doubleValueForUnit(self.mgdlUnit) {
+                //glucoFormatter.forPersonMassUse = true;
+                glucoLocalizedString = "Last Glucose reading: " + mgdl.description
+            } else {
+                println("error reading glucose data")
+            }
+            
+            // 4. Update UI in the main thread
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.lastBgLabel.text = glucoLocalizedString
+                //self.updateBMI()
+                
+            });
+        })
+        
+    }
+    
     func authorizeHealthKit()
     {
         // println("TODO: Request HealthKit authorization")
@@ -37,6 +85,10 @@ class MasterViewController: UITableViewController {
         }
     }
   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateBGValues()
+    }
   
     // MARK: - Segues
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
---
+++ BloodGlucoseAnalytics/BloodGlucoseAnalytics/bgaViewController.swift
@@ -0,0 +1,19 @@
+//
+//  bgaViewController.swift
+//  BloodGlucoseAnalytics
+//
+//  Created by Daniel Diaz on 9/3/15.
+//  Copyright (c) 2015 Daniel Diaz. All rights reserved.
+//
+
+import Foundation
+import HealthKit
+import UIKit
+
+class bgaViewController: UITableViewController {
+    
+    //@IBOutlet weak var lastBGValueLabel: UILabel!
+    //@IBOutlet weak var lastBGLabel: UILabel!
+    
+    
+}
